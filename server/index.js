b79b6c6732425508ab49aa5464294d189b9ce32f
shehrbanoNiazi <120422470+shehrbanoNiazi@users.noreply.github.com>
2023-02-06 07:00:08 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,3 @@
-const express = require("express");
-const cors = require("cors");
-
 const app = express();
 
 app.use(cors());
@@ -9,9 +6,19 @@ app.use(express.json());
 
 const { getCompliment } = require('./controller')
 
+const {getMisFortune} = require('./controller')
+
 const {getFortune} = require('./controller')
+
+const {getLove} = require('./controller')
+
 app.get("/api/compliment", getCompliment);
 
-app.get("/api/fortune", getFortune );
+app.get( "/api/misFortune", getMisFortune);
+
+app.get("/api/fortune", getFortune);
+
+app.get("/api/love", getLove);
 
 app.listen(4000, () => console.log("Server running on 4000"));
+
